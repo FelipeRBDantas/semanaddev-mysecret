0efd2bdf7d88db6e3c12be1f90d0a63b297914ea
feliperbdantas <feliperbdantas@outlook.com>
2024-02-07 22:45:52 -0300

    part 2

--- api/src/domain/dao/DAO.ts
+++ api/src/domain/dao/DAO.ts
@@ -1,4 +1,6 @@
 export default interface DAO<T> {
   create(data: T): Promise<T>;
   findById(id: string): Promise<T | null>;
+  findByEmail(emai: string): Promise<T | null>;
+  update(data: T): Promise<T>;
 }
--- api/src/infra/database/repository/UserRepositoryImpl.ts
+++ api/src/infra/database/repository/UserRepositoryImpl.ts
@@ -1,16 +1,56 @@
 import UserRepository from "@application/repository/UserRepository";
+
 import UserEntity from "@domain/entity/UserEntity";
+import { UserModel } from "@domain/model";
+
+import UserDAO from "@infra/database/dao/UserDAO";
 
 export default class UserRepositoryImpl implements UserRepository {
-  create(user: UserEntity): Promise<UserEntity> {
-    throw new Error("Method not implemented.");
+  constructor(private readonly userDAO: UserDAO) {
+
+  }
+  
+  private toEntity(user: UserModel): UserEntity {
+    return new UserEntity(
+      user.userId,
+      user.name,
+      user.email,
+      user.password,
+      user.pictureUrl,
+      user.createdAt,
+      user.updatedAt
+    );
+  }
+  
+  private toModel(user: UserEntity): UserModel {
+    return {
+      userId: user.userId,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      pictureUrl: user.pictureUrl,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    };
   }
 
-  update(user: UserEntity): Promise<UserEntity> {
-    throw new Error("Method not implemented.");
+  async create(user: UserEntity): Promise<UserEntity> {
+    await this.userDAO.create(this.toModel(user));
+
+    return user;
   }
 
-  findByEmail(email: string): Promise<UserEntity | null> {
-    throw new Error("Method not implemented.");
+  async update(user: UserEntity): Promise<UserEntity> {
+    await this.userDAO.update(this.toModel(user));
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    const user = await this.userDAO.findByEmail(email);
+
+    if (!user) return null;
+
+    return user;
   }
 }
