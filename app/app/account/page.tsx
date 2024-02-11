f0dc12ec9736760f439f8762e152c31ac7479a2b
feliperbdantas <feliperbdantas@outlook.com>
2024-02-10 23:11:35 -0300

    part 4

---
+++ app/app/account/page.tsx
@@ -0,0 +1,11 @@
+export default function AccountPage() {
+  return (
+    <div>
+      <h1>Account</h1>
+      <p>perguntas e respostas anônimas</p>
+      <p>vamos começar</p>
+      <button>criar uma conta</button>
+      <button>acessar minha conta</button>
+    </div>
+  );
+}
