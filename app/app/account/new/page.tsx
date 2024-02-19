5fae46f79ab94db1a861cfbcb6f13c6a891fe8f7
feliperbdantas <feliperbdantas@outlook.com>
2024-02-19 20:38:07 -0300

    part 5

--- app/app/account/new/page.tsx
+++ app/app/account/new/page.tsx
@@ -1,43 +1,61 @@
+'use client';
+
+import { useState } from "react";
+
 import Header from "@/app/components/header";
 
 export default function NewAccountPage() {
+  const [step, setStep] = useState(1);
+
+  const updateStep = (action: 'next' | 'back') => {
+    if (action === 'next') setStep(step + 1);
+    if (action === 'back') setStep(step - 1);
+  }
+
   return (
     <>
       <Header />
       <h1 className="mb-[60px]">perguntas e mensagens anônimas</h1>
       <form className="flex flex-col w-full space-y-[15px]">
-        <FormEmail />
+        { step === 1 && <FormEmail updateStep={updateStep} /> }
+        { step === 2 && <FormPassword updateStep={updateStep} /> }
+        { step === 3 && <FormProfile updateStep={updateStep} /> }
       </form>
     </>
   );
 }
 
-function FormEmail() {
+function FormEmail({ updateStep }: { updateStep: (action: 'next' | 'back') => void }) {
   return (
     <>
-      <input type="text" placeholder="informe seu email" />
-      <button className="button">continuar</button>
-      <a className="button-secondary">voltar</a>
+      <input type="email" placeholder="informe seu email" />
+      <button className="button" type="button" onClick={() => updateStep('next')}>continuar</button>
+      <a href="/" className="button-secondary">voltar</a>
     </>
   );
 }
 
-function FormPassword() {
+function FormPassword({ updateStep }: { updateStep: (action: 'next' | 'back') => void }) {
   return (
     <>
-      <input type="text" placeholder="informe sua senha" />
-      <button className="button">continuar</button>
+      <input type="password" placeholder="informe sua senha" />
+      <button className="button" type="button" onClick={() => updateStep('next')}>continuar</button>
+      <button className="button-secondary" onClick={() => updateStep('back')}>voltar</button>
     </>
   );
 }
 
-function FormProfile() {
+function FormProfile({ updateStep }: { updateStep: (action: 'next' | 'back') => void }) {
   return (
     <>
       <input type="text" placeholder="informe seu nome" />
-      <input type="text" placeholder="informe seu usuário" />
+      <div className="inputGroup">
+        <span>@</span>
+        <input type="text" placeholder="informe seu usuário" />
+      </div>
       <FormAvatar />
-      <button className="button">continuar</button>
+      <button className="button" type="button" onClick={() => updateStep('next')}>continuar</button>
+      <button className="button-secondary" onClick={() => updateStep('back')}>voltar</button>
     </>
   );
 }
