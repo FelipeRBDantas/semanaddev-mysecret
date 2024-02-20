b7bc8d95a9c11c1343aba319a590b58da6889db4
feliperbdantas <feliperbdantas@outlook.com>
2024-02-20 20:38:55 -0300

    part 5

--- app/app/account/login/page.tsx
+++ app/app/account/login/page.tsx
@@ -1,15 +1,29 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+
 import Header from "@/app/components/header";
 
 export default function LoginPage() {
+  const router = useRouter();
+  
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    // const formData = new FormData(e.currentTarget);
+
+    router.push('/dash');
+  }
+
   return (
     <>
       <Header />
       <h1 className="mb-[60px]">perguntas e mensagens anônimas</h1>
-      <form className="flex w-full flex-col space-y-[15px]">
+      <form onSubmit={ handleSubmit } className="flex w-full flex-col space-y-[15px]">
         <input type="text" placeholder="informe seu email" />
         <input type="text" placeholder="informe sua senha" />
         <button type="submit" className="button">acessar minha conta</button>
-        <a className="button-secondary">voltar</a>
+        <a href="/" className="button-secondary">voltar</a>
       </form>
     </>
   );
--- app/app/dash/layout.tsx
+++ app/app/dash/layout.tsx
@@ -1,21 +1,44 @@
+'use client';
+
+import Image from 'next/image';
+
+import Styles from './styles.module.css';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="flex w-full justify-between mb-4">
-        <button>Sair</button>
-        <span>Logo</span>
-        <span>Avatar</span>
+      <header className={Styles.header}>
+        <button>
+          <Image src="/icon/log-out.svg" alt="Sair" width={24} height={24} />
+        </button>
+        <Image src="/logo.png" alt="MySecret" width={32} height={32} className='opacity-50' />
+        <Image src="/avatar/avatar5.png" alt="Avatar" width={48} height={48} />
       </header>
       <nav className="flex w-full mb-4">
         <ul className="flex w-full justify-between">
           <li>
-            <NavButton href="/dash">home</NavButton>
+            <NavButton href="/dash">
+              <>
+                <Image src="/icon/message.svg" alt="Perguntas" width={16} height={16} />
+                perguntas
+              </>
+            </NavButton>
           </li>
           <li>
-            <NavButton href="/dash/messages">messages</NavButton>
+            <NavButton href="/dash/messages">
+              <>
+                <Image src="/icon/inbox.svg" alt="Mensagens" width={16} height={16} />
+                mensagens
+              </>
+            </NavButton>
           </li>
           <li>
-            <NavButton href="/dash/answers">respostas</NavButton>
+            <NavButton href="/dash/answers">
+              <>
+                <Image src="/icon/send.svg" alt="Respostas" width={16} height={16} />
+                respostas
+              </>
+            </NavButton>
           </li>
         </ul>
       </nav>
@@ -24,10 +47,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-function NavButton({ href, children }: { href: string; children: string }) {
+function NavButton({ href, children }: { href: string; children: React.ReactNode }) {
   return (
     <a 
-      className="bg-[#D43A72] text-white rounded-[14px] text-[12px] p-3" 
+      className="flex bg-[#D43A72] text-white rounded-[14px] text-[12px] p-3" 
       href={ href }
     >
       { children }
